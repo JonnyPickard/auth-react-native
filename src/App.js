673be77b6a6d9ec5ee8d62584a704897bcbd51ab
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-16 21:21:34 +0100

    Add basic scaffolding for login form card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAPI87JxCdPgPFFNnGTMJWVJkns6i9Ein8',
+      authDomain: 'authreactnative-9016b.firebaseapp.com',
+      databaseURL: 'https://authreactnative-9016b.firebaseio.com',
+      projectId: 'authreactnative-9016b',
+      storageBucket: 'authreactnative-9016b.appspot.com',
+      messagingSenderId: '545728647170',
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     );
   }
