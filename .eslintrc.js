dd7b9341868fdd9fa94218db1788316b0e2d4979
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-16 21:00:21 +0100

    Add common components

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   "extends": "airbnb",
   "rules": {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/prefer-stateless-function": 0
+    "react/prefer-stateless-function": 0,
+    "import/prefer-default-export": 0,
+    "no-use-before-define": 0,
+    "react/prop-types": 0
   }
 };
