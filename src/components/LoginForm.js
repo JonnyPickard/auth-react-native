04b4023feb8468bbb8584d343bb9e74b111be0d3
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-17 22:36:45 +0100

    Add login success & failure handling methods

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,12 +15,30 @@ class LoginForm extends Component {
     this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication Failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(err => this.onLoginFailure(err.message));
       });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      loading: false,
+      email: '',
+      password: '',
+      error: '',
+    });
+  }
+
+  onLoginFailure(err) {
+    this.setState({
+      loading: false,
+      email: '',
+      password: '',
+      error: err,
+    });
   }
 
   renderButton() {
